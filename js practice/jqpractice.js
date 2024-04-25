90703f5bc050e107201e20b35765c646ccc1177a
ebillinfo1 <129174851+ebillinfo1@users.noreply.github.com>
2024-04-25 12:03:16 +0500

    update on 25/04
    update

---
+++ js practice/jqpractice.js
@@ -0,0 +1,36 @@
+$(function(){
+    //jq will execute this function after page load
+    $("#addbutton").click(handlebtnclick);
+    //$("#todos li").click(removeMe);
+    $("#todos").on("click", "li", removeMe);
+    
+    });
+    function handlebtnclick()
+    {
+       // alert("btn clicked");
+       var newtodo = $("#newtodo").val();
+       if(!newtodo){
+        $("#newtodo").addClass("error");
+        return;
+       }
+       $("#newtodo").removeClass("error");
+       //$("#newtodo").val("");
+       $("#todos").append("<li>" + newtodo + "</li>");
+       //$("#todos li").click(removeMe);
+    }
+    function removeMe(){
+        $(this).remove();
+        //$(this).fadeOut();
+    }
+    /*
+    Today we Covered 
+    - Events
+    - Events buinding
+    - Validation
+    - event deligation
+    - Return
+
+    Class Work
+    Implement this code as it is, after that change the tag and use "td" tag
+    to adtodo instead of "li".
+    */
